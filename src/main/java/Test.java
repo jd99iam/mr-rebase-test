c6e1d212cd6c90550e1aab771360b7c335f8f37d
jd99iam <jidamine87593@gmail.com>
2026-04-29 00:11:16 +0900

    hello 커밋2

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,5 +4,6 @@ public class Test {
         System.out.println("커밋2");
         System.out.println("커밋3");
         System.out.println("hello 커밋1");
+        System.out.println("hello 커밋2");
     }
 }
